c3067260cbc59ce5f40ad8e699ece6736eef0876
Matthew Lawrence <mattblawr@gmail.com>
2020-02-29 11:47:02 -0800

    Cog updates

--- TimeGame/Assets/Scripts/Shadow.cs
+++ TimeGame/Assets/Scripts/Shadow.cs
@@ -14,9 +14,22 @@ public class Shadow : MonoBehaviour
     [Tooltip("The y position that the object will be set to.")]
     public float yValue = 0.01f;
 
+    public bool lockRotation = false;
+    Quaternion startRot;
+
+    private void Start()
+    {
+        startRot = transform.rotation;
+    }
+
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(transform.position.x, yValue, transform.position.z);
+
+        if (lockRotation)
+        {
+            transform.rotation = startRot;
+        }
     }
 }
