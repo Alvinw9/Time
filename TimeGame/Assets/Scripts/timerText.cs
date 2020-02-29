4d69b445a6ca3f70467aeda2f73c1997bf98f375
BrandonFoey <bfoey@ucsd.edu>
2020-02-28 21:45:36 -0800

    ui stuff

---
+++ TimeGame/Assets/Scripts/timerText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerText : MonoBehaviour
+{
+    Text text;
+    float startGameTime = 60;
+    float gameTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        gameTimer = startGameTime;
+        text.text = "Time: " + gameTimer.ToString("F2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTimer -= Time.deltaTime;
+        text.text = "Timer: " + gameTimer.ToString("F2");
+
+        if(gameTimer == 0)
+        {
+
+        }
+
+    }
+
+    void changeTime(float change)
+    {
+        gameTimer += change;
+    }
+}
