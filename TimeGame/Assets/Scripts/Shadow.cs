55ec542fc0a8f69f96a6b8981772073c1fe53d0f
Matthew Lawrence <mattblawr@gmail.com>
2020-02-29 11:23:54 -0800

    Wall physics material

--- TimeGame/Assets/Scripts/Shadow.cs
+++ TimeGame/Assets/Scripts/Shadow.cs
@@ -1,9 +1,17 @@
-﻿using System.Collections;
+﻿/*
+ * For VGDC Winter Game Jam
+ * Author: Matthew Lawrence
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Sets this object's y position to the give value in world space every frame. 
+/// </summary>
 public class Shadow : MonoBehaviour
 {
+    [Tooltip("The y position that the object will be set to.")]
     public float yValue = 0.01f;
 
     // Update is called once per frame
